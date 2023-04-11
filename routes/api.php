3792742fd6231e2c28418d7eef735416bcbbdfcb
DanielSitumeang7 <[dsitumeang7@gmail.com]>
2023-04-11 15:49:01 +0700

    autentikasi controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PostController;
+use App\Http\Controllers\Autentikasi;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/ambilPost', [PostController::class, 'ambilSemnuaPost']);
+Route::post('/registrasi', [Autentikasi::class, 'register']);
+Route::post('/login', [Autentikasi::class, 'login']);
+Route::post('/logout', [Autentikasi::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get('/posts', [PostController::class, 'ambilSemnuaPost'])->middleware('auth:sanctum');
 Route::get('/posts/{id}', [PostController::class, 'ambilPostSpesifik']);
 Route::post('/posts', [PostController::class, 'tambahPost']);
 Route::put('/posts/{id}', [PostController::class, 'ubahPost']);
